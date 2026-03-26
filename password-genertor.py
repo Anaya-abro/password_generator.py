885504458cb686ffcf122072dc4c6eb1091d3146
Anaya_Abro <abroayaan8@gmail.com>
2026-03-27 00:47:40 +0500

    Add files via upload

---
+++ password-genertor.py
@@ -0,0 +1,15 @@
+import random
+
+length = int(input('Enter the length of your password:')) 
+
+characters = 'abcdefghijklmnopqrstuvwxyz1234567890@#$%^&*!'
+
+password = ''
+
+for i in range(length):
+
+    password += random.choice(characters)
+print('Your password is:',password)
+
+
+
